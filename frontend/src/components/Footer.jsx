2e5866015d4d439c81353b6ca9abe89dd20019b4
DeepakPrakasam <deepakp.22cse@kongu.edu>
2025-05-23 13:29:46 +0530

    Updated

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -20,7 +20,7 @@ function Footer() {
           </div>
 
           {/* Useful Links */}
-          <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+          {/* <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
             <h5 className="text-uppercase mb-4 font-weight-bold">
               Useful Links
             </h5>
@@ -54,35 +54,35 @@ function Footer() {
                 Find a Store
               </a>
             </p>
-          </div>
+          </div> */}
 
           {/* Information */}
           <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
             <h5 className="text-uppercase mb-4 font-weight-bold">
               Information
             </h5>
-            <p>
+            {/* <p>
               <a href="#" className="text-white text-decoration-none">
                 Blog
               </a>
-            </p>
-            <p>
+            </p> */}
+            {/* <p>
               <a href="#" className="text-white text-decoration-none">
                 Offers & Contest Details
               </a>
-            </p>
-            <p>
+            </p> */}
+            {/* <p>
               <a href="#" className="text-white text-decoration-none">
                 Help & FAQs
               </a>
-            </p>
+            </p> */}
             <p>
-              <a href="#" className="text-white text-decoration-none">
+              <a href="/ourstory" className="text-white text-decoration-none">
                 About Dhandapani Jewellery
               </a>
             </p>
             <p>
-              <a href="#" className="text-white text-decoration-none">
+              <a href="/ourstory" className="text-white text-decoration-none">
                 Contact Us
               </a>
             </p>
